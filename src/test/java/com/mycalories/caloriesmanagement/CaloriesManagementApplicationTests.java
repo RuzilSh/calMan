1d5d660ec175f6bf7c8a251ff6966669b39b27f3
Ruzil Sh <hloremon1991@gmail.com>
2020-05-05 23:50:01 -0400

    begining my project

---
+++ src/test/java/com/mycalories/caloriesmanagement/CaloriesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycalories.caloriesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
