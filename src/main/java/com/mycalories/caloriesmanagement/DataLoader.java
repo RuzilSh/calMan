bef55e48d682618e805eef4f0ea39cc771b924bb
Ruzil Sh <hloremon1991@gmail.com>
2020-06-06 01:04:56 -0700

    added meals

--- src/main/java/com/mycalories/caloriesmanagement/DataLoader.java
+++ src/main/java/com/mycalories/caloriesmanagement/DataLoader.java
@@ -1,7 +1,10 @@
 package com.mycalories.caloriesmanagement;
 
+import com.mycalories.caloriesmanagement.controller.SecurityUtil;
 import com.mycalories.caloriesmanagement.model.Meal;
+import com.mycalories.caloriesmanagement.model.User;
 import com.mycalories.caloriesmanagement.repository.MealRepository;
+import com.mycalories.caloriesmanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -15,23 +18,26 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     MealRepository mealRepository;
 
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-         mealRepository.save(new Meal(LocalDateTime.of(2020,1,25,13,15), "sandwich", 700));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,1,25,20,00), "ice cream", 400));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,1,25,23,30), "breakfast", 750));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,2,14,9,30), "lunch", 1000));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,2,14,15,15), "yummy", 700));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,2,14,20,00), "dinner", 800));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,3,5,6,45), "pizza", 1000));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,3,5,16,15), "soup", 500));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,3,5,23,30), "burrito", 400));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,4,30,10,45), "chicken", 600));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,4,30,17,15), "hamburger", 800));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,4,30,20,30), "salad", 600));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,5,3,5,45), "spaghetti", 350));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,5,3,15,00), "hot dog", 450));
-         mealRepository.save(new Meal(LocalDateTime.of(2020,5,3,22,15), "vegetables", 550));
+         userRepository.save(new User(SecurityUtil.USER_CALORIES));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,1,7,13,15), "Sichuan Pork", 700));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,1,7,20,00), "Hot pot", 400));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,1,7,23,30), "Braised pork balls in gravy", 750));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,2,14,9,30), "Tandoori Chicken", 1000));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,2,14,15,15), "Murg Makhani", 700));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,2,14,20,00), "Tuna sandwich", 800));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,3,21,6,45), "Yummy pizza", 1000));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,3,21,16,15), "Apple Pie", 500));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,3,21,23,30), "Burrito", 400));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,4,30,10,45), "Mediterranean salad", 600));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,4,30,17,15), "French fries", 800));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,4,30,20,30), "Spaghetti with shrimp", 700));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,5,25,5,45), "Seaweed salad", 350));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,5,25,15,00), "Sushi", 450));
+         mealRepository.save(new Meal(LocalDateTime.of(2020,5,25,22,15), "Miso soup", 550));
     }
 }
